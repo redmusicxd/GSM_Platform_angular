1937b9c166696f6315b14aed8f2463c381079320
RedCode <dannegru40@gmail.com>
2020-10-21 00:41:53 +0300

    Text to Field,  more transitioning to FormGroup and minor design adjusting

--- src/app/checkorder/checkorder.component.ts
+++ src/app/checkorder/checkorder.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NotifierService } from 'angular-notifier';
 import { ApiService } from '../api.service';
 import { Order, OrderInterface } from "../regorder/regorder.component"
 
@@ -12,10 +14,22 @@ export class CheckorderComponent implements OnInit {
   noder: number;
   data: OrderInterface;
   notfound: boolean;
+  editmode: boolean = false;
+  editfieldform: FormGroup;
+  admin: boolean = false;
+  private notifier: NotifierService;
+  constructor(private api: ApiService, private fb: FormBuilder, notifier: NotifierService) {
+    this.notifier = notifier;
+  }
 
-  constructor(private api: ApiService) { }
+  showNotifs(type:string, message: string){
+    this.notifier.notify(type, message);
+  }
 
   ngOnInit(): void {
+    if(localStorage.getItem('jwt') && localStorage.getItem('role') === "Admin"){
+      this.admin = true;
+    }
   }
   submit(f: KeyboardEvent){
     if(f.key == "Enter"){
@@ -25,10 +39,20 @@ export class CheckorderComponent implements OnInit {
       this.data = new Order();
     }
   }
+  editField(){
+    this.editmode = true;
+  }
   searchOrder(ordern: number) {
     this.notfound = false;
     this.data = null;
-    this.api.getOrder(ordern).subscribe((order: OrderInterface) => this.data = order, (err : any) => err.status == 404 ? this.notfound = true : this.notfound = false
+    this.api.getOrder(ordern).subscribe((order: OrderInterface) => {this.data = order; this.editfieldform = this.fb.group(this.data)}, (err : any) => err.status == 404 ? this.notfound = true : this.notfound = false
     );
+  }
+  updateOrder(orderid: any){
+    this.api.modifyOrder(orderid,this.editfieldform.value,localStorage.getItem('jwt')).subscribe((res: OrderInterface) => {
+    this.editmode = false;
+    this.data = this.editfieldform.value;
+    this.showNotifs("success",`[CheckOrder] Comanda ${res.id} a fost modificata cu success`)
+    }, err => this.showNotifs("error",`[CheckOrder] Comanda ${this.noder} nu a putut fi modificata`))
   }
 }
