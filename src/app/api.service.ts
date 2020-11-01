1d6e68b6abad9d998a9447c3758e7a45aa4a9fa1
RedCode <dannegru40@gmail.com>
2020-11-01 11:11:34 +0200

    switch to dotenv

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,44 +9,42 @@ import { environment } from 'src/environments/environment';
 
 export class ApiService {
 
-  base: string = environment.production ? `${environment.API_URL}/api` : `${environment.API_URL}`;
-
   constructor(private api: HttpClient) {} 
 
   getOrder(order: number){
-    return this.api.get(`${this.base}/comandas/${order}`);
+    return this.api.get(`${environment.API_URL}/comandas/${order}`);
   }  
   getOrders(jwt: string){
-    return this.api.get(`${this.base}/comandas?_sort=id:DESC`, {headers : {
+    return this.api.get(`${environment.API_URL}/comandas?_sort=id:DESC`, {headers : {
       Authorization: `Bearer ${jwt}`
     }});
   }
   searchRelatedOrders(phone: number, id: number, jwt: string){
-    return this.api.get(`${this.base}/comandas?_sort=id:DESC&phone_number=${phone}&id_ne=${id}`, {headers : {
+    return this.api.get(`${environment.API_URL}/comandas?_sort=id:DESC&phone_number=${phone}&id_ne=${id}`, {headers : {
       Authorization: `Bearer ${jwt}`
     }});
   }
   deleteOrder(order:number, jwt: string){
-    return this.api.delete(`${this.base}/comandas/${order}`, {headers : {
+    return this.api.delete(`${environment.API_URL}/comandas/${order}`, {headers : {
       Authorization: `Bearer ${jwt}`
     }});
   }  
   modifyOrder(orderid: number, order: OrderInterface, jwt: string){
-    return this.api.put(`${this.base}/comandas/${orderid}`, order, {headers : {
+    return this.api.put(`${environment.API_URL}/comandas/${orderid}`, order, {headers : {
       Authorization: `Bearer ${jwt}`
     }});
   }
   login(username: string, password: string){
-    return this.api.post(`${this.base}/auth/local`,{identifier: username, password:password})
+    return this.api.post(`${environment.API_URL}/auth/local`,{identifier: username, password:password})
   }
 
   getUserInfo(jwt: string){
-    return this.api.get(`${this.base}/users/me`, {headers : {
+    return this.api.get(`${environment.API_URL}/users/me`, {headers : {
       Authorization: `Bearer ${jwt}`
     }})
   }
   registerOrder(jwt: string, order: OrderInterface){
-    return this.api.post(`${this.base}/comandas`,order,{headers: {
+    return this.api.post(`${environment.API_URL}/comandas`,order,{headers: {
       Authorization: `Bearer ${jwt}`
     }})
   }
